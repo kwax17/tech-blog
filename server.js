ac84874bdca46a24ec8ba61181977a65a9bf6dbd
Kelly Wacker <kelwacker@gmail.com>
2022-09-07 14:04:53 -0400

    idk

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const routes = require('./controllers');
+
 const sequelize = require("./config/config");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
