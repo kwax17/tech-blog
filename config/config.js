ae5b1780b130f0b9580b9cd882a12597d4427a02
Kelly Wacker <kelwacker@gmail.com>
2022-09-07 14:17:12 -0400

    oops

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: '127.0.0.1',
       dialect: 'mysql',
-      // port: 3306,
+      port: 3306,
     });
 
 sequelize.sync({});
