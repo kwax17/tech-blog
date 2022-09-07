3df2271a7d4488972219db2890b936f53064fcd1
Kelly Wacker <kelwacker@gmail.com>
2022-09-06 22:45:17 -0400

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const routes = require('./controllers');
 const sequelize = require("./config/config");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -29,9 +29,9 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
